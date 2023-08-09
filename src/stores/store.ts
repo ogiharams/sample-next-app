ca5601538f17628f6d0eda5cc9d64e819b7de5a8
ogi <ogihara.masataka@gmail.com>
2023-08-09 21:55:23 +0900

    CreateSlice

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -5,6 +5,7 @@ import fetchReducer from "./slices/fetch/fetchSlice";
 import fetch2Reducer from "./slices/fetch2/fetch2Slice";
 import rtkLoginReducer from "./slices/rtkTaskLogin/loginSlice";
 import rtkTaskReducer from "./slices/rtkTaskTask/taskSlice";
+import custiomCounterReducer from "./slices/rtkLesson/custiomCounterSlice";
 
 export const store = configureStore({
   reducer: {
@@ -14,6 +15,7 @@ export const store = configureStore({
     fetch2: fetch2Reducer,
     rtkLogin: rtkLoginReducer,
     rtkTask: rtkTaskReducer,
+    custiomCounter: custiomCounterReducer,
   },
 });
 export type AppDispatch = typeof store.dispatch;
